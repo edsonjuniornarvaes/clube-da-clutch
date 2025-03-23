48db4f3ab26126e84a6509e774ce7d8da7df591b
Edson Junior <edsonjunior.narvaes@gmail.com>
2025-03-23 00:17:55 -0300

    feat: add auth

---
+++ app/(public)/_layout.tsx
@@ -0,0 +1,27 @@
+import { Stack } from "expo-router";
+
+export default function PublicLayout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#121212",
+        },
+        headerTintColor: "#FFF",
+      }}
+    >
+      <Stack.Screen
+        name="sign-in/view"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="register"
+        options={{
+          headerTitle: "Criar conta",
+        }}
+      />
+    </Stack>
+  );
+}
